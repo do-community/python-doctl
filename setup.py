7bb3c0b02d34da10e5f72d5a4adf62199f1f189a
Kenneth Reitz <me@kennethreitz.org>
2018-05-29 14:33:54 -0400

    v0.1.1
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/kennethreitz/doctl"
 EMAIL = "me@kennethreitz.org"
 AUTHOR = "Kenneth Reitz"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["requests", "requests-html", "maya", "appdirs", "delegator.py"]
